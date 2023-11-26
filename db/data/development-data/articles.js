a9ee79b504558ab5be2414df616561f46457ed76
DeanDevine <deanrussondevine@gmail.com>
2023-11-26 00:24:06 +0000

    update package.json for render

--- db/data/development-data/articles.js
+++ db/data/development-data/articles.js
@@ -17,4 +17,22 @@ module.exports = [
     article_img_url:
       "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
   },
+  {
+    title: "Best Gym In The World?",
+    body: "We think we've found what could be the best gym in the world...",
+    article_img_url:
+      "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
+  },
+  {
+    title: "3 Day Splits vs 4 Day Splits",
+    body: "We compare 3 day splits against 4 day splits...",
+    article_img_url:
+      "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
+  },
+  {
+    title: "How To Mentally Prepare For Leg Day",
+    body: "Having trouble with committing to training legs?",
+    article_img_url:
+      "https://images.pexels.com/photos/158651/news-newsletter-newspaper-information-158651.jpeg?w=700&h=700",
+  },
 ];
